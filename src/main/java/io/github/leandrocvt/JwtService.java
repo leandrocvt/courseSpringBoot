8e3823c938a4e96cd5819e06df7bc3796865ae00
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2023-03-30 15:32:08 -0300

    Generating the Token

--- src/main/java/io/github/leandrocvt/JwtService.java
+++ src/main/java/io/github/leandrocvt/JwtService.java
@@ -1,10 +1,48 @@
 package io.github.leandrocvt;
 
+import io.github.leandrocvt.domain.entities.UserModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.tomcat.jni.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
+    @Value("${security.jwt.expiration}")
     private String expiration;
-    private String key;
+
+    @Value("{$security.jwt.signature-key}")
+    private String signatureKey;
+
+    public String generateToken(UserModel userModel){
+        long expString = Long.valueOf(expiration);
+        LocalDateTime dateTimeExpiration = LocalDateTime.now().plusMinutes(expString);
+        Instant instant = dateTimeExpiration.atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instant);
+
+        return Jwts
+                .builder()
+                .setSubject(userModel.getLogin())
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, signatureKey )
+                .compact();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(VendasApplication.class);
+        JwtService service = context.getBean(JwtService.class);
+        UserModel userModel = UserModel.builder().login("leandro").build();
+        String token = service.generateToken(userModel);
+        System.out.println(token);
+    }
+
 }
